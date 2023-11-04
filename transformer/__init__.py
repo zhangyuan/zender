71560737b9e8cb694c0568b145b76c6ab48f77d7
Yuan Zhang <zhangyuan@users.noreply.github.com>
2023-11-04 17:14:48 +0800

    add transfomer

---
+++ transformer/__init__.py
@@ -0,0 +1,85 @@
+from jinja2 import Environment, FileSystemLoader
+import os
+from pathlib import Path
+import json
+
+class MetadataStore:
+  def __init__(self) -> None:
+    self.lineage = []
+
+  def add_lineage(self, lineage):
+    self.lineage.append(lineage)
+
+  def to_json(self):
+    return json.dumps({
+      "lineage": lineage.as_dict() for lineage in self.lineage
+    }, indent=4)
+
+class Lineage:
+  def __init__(self, source) -> None:
+    self.source = source
+    self.dependencies = []
+
+  def ref(self, dependence):
+    if dependence not in self.dependencies:
+      self.dependencies.append(dependence)
+
+    return dependence
+
+  def as_dict(self):
+    return {
+      "source": self.source,
+      "depedencies": self.dependencies
+    }
+
+  def __str__(self):
+    return json.dumps(self.__dict__)
+
+class Compiler:
+  def __init__(self, search_directory, target_path: str, metadata_store: MetadataStore) -> None:
+    self.env = Environment(
+        loader=FileSystemLoader(search_directory),
+        autoescape=False
+    )
+    self.target_directory = target_path
+    self.metadata_store = metadata_store
+
+  def compile(self, model_path):
+    lineage = Lineage(model_path)
+    func_dict = {
+      "ref": lineage.ref
+    }
+
+    template = self.env.get_template(model_path)
+    template.globals.update(func_dict)
+
+    content = template.render()
+
+    if not os.path.exists(self.target_directory):
+      os.makedirs(self.target_directory)
+
+    targetFilePath = Path(os.path.join(self.target_directory, "compiled", model_path))
+    targetFileDirectory = targetFilePath.parent
+
+    if not os.path.exists(targetFileDirectory):
+      os.makedirs(targetFileDirectory)
+
+    with open(targetFilePath, "w") as f:
+      f.write(content)
+
+    self.metadata_store.add_lineage(lineage=lineage)
+
+
+  def save_metadata(self):
+    metadata_store_path = os.path.join(self.target_directory, "metadata.json")
+    with open(metadata_store_path, "w") as f:
+      f.write(self.metadata_store.to_json())
+
+
+def compile_models(models):
+  metadata_store = MetadataStore()
+  compiler = Compiler(search_directory=["models", "macros"], target_path="target", metadata_store=metadata_store)
+
+  for model in models:
+    compiler.compile(model)
+  compiler.save_metadata()

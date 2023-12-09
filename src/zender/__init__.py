32bd56cf1e0e9ef25e555d447a1c3ab57389021c
Yuan Zhang <zhangyuan@users.noreply.github.com>
2023-12-09 23:32:57 +0800

    create directory metadata_store_path if it is not existing

--- src/zender/__init__.py
+++ src/zender/__init__.py
@@ -85,5 +85,7 @@ class Compiler:
 
     def save_metadata(self):
         metadata_store_path = os.path.join(self.target_directory, "metadata.json")
+        if not os.path.exists(metadata_store_path):
+            os.makedirs(os.path.dirname(metadata_store_path))
         with open(metadata_store_path, "w", encoding="utf-8") as f:
             f.write(self.metadata_store.to_json())
